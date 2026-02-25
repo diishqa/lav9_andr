87305d7ab099e29bbcf643f4622f74c3095a2668
Alina <a72658695@gmail.com>
2026-02-25 09:23:53 +0300

    кнопи доб, все вроде еÑчетко

--- app/src/main/java/com/startseva/andr_kuz/MainActivity.kt
+++ app/src/main/java/com/startseva/andr_kuz/MainActivity.kt
@@ -5,19 +5,28 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.currentCompositionContext
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -39,7 +48,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color= MaterialTheme.colorScheme.background
                 ) {
-                    SerialCatalogApp()
+                    SerialButtons()
                 }
             }
         }
@@ -49,7 +58,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun SerialCard(serial: Serial, modifier: Modifier = Modifier) {
     Card(modifier=modifier) {
-        Column{
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
             Image(
                 painter = painterResource(serial.imageResourceId),
                 contentDescription = stringResource(serial.titleResourceId),
@@ -98,13 +109,41 @@ private fun SerialCardPreview() {
             Serial(
                 R.string.ser1,
                 R.string.ser1_description,
-                R.drawable.tigr
+                R.drawable.ser1
             )
         )
     }
 }
 @Composable
 fun SerialButtons() {
+    val serialList = Datasource().loadSerialList()
+    var serialIndex by remember { mutableStateOf(0) }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        SerialCard(serial = serialList[serialIndex])
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Button(
+                onClick = {
+                    if (serialIndex > 0) serialIndex--
+                }
+            ) {
+                Text(text = "назад")
+            }
+            Button(
+                {
+                    if (serialIndex < serialList.size - 1) serialIndex++
+                }
+            ) {
+                Text(text="вперед")
+            }
+        }
 
+    }
 }
 
